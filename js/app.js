fe2f82135d38784752f3ca33debd989e52634618
CalabresAC <carter.ash@gmail.com>
2017-08-22 15:27:14 -0400

    First Commit of Project

---
+++ js/app.js
@@ -0,0 +1,89 @@
+
+// Enemies our player must avoid
+class Enemy {
+    constructor(x,y,speed) {
+        this.sprite = 'images/enemy-bug.png';
+        this.x = -40;
+        this.y = Math.floor(Math.random() * 201) + 50;
+        this.speed = Math.floor(Math.random() * 101) + 65;
+        console.log(this.y);
+    }
+    update(dt) {
+            // You should multiply any movement by the dt parameter
+            // which will ensure the game runs at the same speed for
+            // all computers.
+            this.x = this.x + this.speed * dt;
+            //the following resets the enemy's position to the left side
+            //of the board and randomizes the speed as well as the x & y coordinates.
+            if (this.x > 505) {
+            this.x = -40;
+            this.y = Math.floor(Math.random() * 201) + 50;
+            this.speed = Math.floor(Math.random() * 101) + 50;
+            console.log(this.y);
+        }
+    }
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+class Player {
+    constructor(x,y,movement) {
+        this.sprite = 'images/char-boy.png';
+        this.x = 300;
+        this.y = 300;
+    }
+    handleInput(direction) {
+        if (direction === 'up') {
+            this.x += 100;
+        }
+        if (direction === 'down') {
+            this.x -= 100;
+        }
+        if (direction === 'right') {
+            this.y += 100 ;
+        }
+        if (direction === 'left') {
+            this.y -= 100;
+        }
+    }
+    update(dt) {
+            // You should multiply any movement by the dt parameter
+            // which will ensure the game runs at the same speed for
+            // all computers.
+            this.x = this.x + this.speed * dt;
+    }
+    render() {
+            ctx.drawImage(Resources.get(this.sprite),this.x, this.y);
+    }
+
+}
+
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+var bug1 = new Enemy();
+var bug2 = new Enemy();
+var bug3 = new Enemy();
+
+var allEnemies = [bug1, bug2, bug3];
+var player = new Player();
+console.log(player);
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
